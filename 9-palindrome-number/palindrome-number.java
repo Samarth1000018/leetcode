c9eb6cdd23a4b8169a36e6e742df84a2f97b0399
Samarth S S <218992563+Samarth1000018@users.noreply.github.com>
2026-09-25 23:05:15 +0530

    Time: 5 ms (83.96%) | Memory: 46 MB (52.65%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -3,15 +3,14 @@ class Solution {
         if(x<0){
             return false;
         }
-
         int reverse = 0;
-        int xcopy = x;
+        int num = x;
 
-        while(x>0){
-            reverse = (reverse * 10) + (x%10);
-            x/=10;
+        while(num != 0){
+            reverse = reverse * 10 + num %10;
+            num = num / 10;
         }
-        return reverse == xcopy;
+        return(reverse == x);
     }
 }
 
